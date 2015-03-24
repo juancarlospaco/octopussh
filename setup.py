8397dceb7032915f61bccfdeeabe425e72ec47e2
juancarlospaco <juancarlospaco@gmail.com>
2015-03-24 20:32:25 -0300

    Clean out

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# To Upload to PyPI by executing:  python3 setup.py sdist upload -r pypi
+
+
+"""Setup.py for Python, as Generic as possible."""
 
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 MODULE_PATH = os.path.join(os.getcwd(), "octopussh")
@@ -15,21 +20,15 @@ def find_this(search, filename=MODULE_PATH):
     if not search:
         return
     for line in open(str(filename)).readlines():
-        if search in line:
+        if search.lower() in line.lower():
             line = line.split("=")[1].strip()
-            if search in "__version__":
-                return line.replace("'", "").replace('"', '')
+            if "'" in line or '"' in line or '"""' in line:
+                line = line.replace("'", "").replace('"', '').replace('"""', '')
             return line
 
 
-try:
-    long_description = open("README.rst").read()
-except IOError:
-    long_description = ""
-
-
-
 setup(
+
     name="octopussh",
     description="SSH Launcher from .sh Bash Scripts",
 
@@ -44,15 +43,17 @@ setup(
     maintainer_email=find_this("__email__"),
 
 
-    packages=find_packages(),
-    install_requires=[],
-    scripts=['octopussh'],
-
-    long_description=long_description,
-
     include_package_data=True,
     zip_safe=True,
+    install_requires=['pip'],
+    requires=['pip'],
 
+
+    scripts=['octopussh'],
+    entry_points = {'console_scripts': ['octopussh = octopussh']},
+
+
+    keywords=['SSH', 'Bash', 'SysAdmin', 'DevOps', 'Administration', 'Server'],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
